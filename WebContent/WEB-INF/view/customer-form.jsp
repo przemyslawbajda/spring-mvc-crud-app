efdc0f314ed5ec33ec22a2bf8505e3ac93adfa9a
przemyslawbajda <przemek.android20@gmail.com>
2021-08-05 12:29:42 +0200

    add customer form page and necessary logic to show it

---
+++ WebContent/WEB-INF/view/customer-form.jsp
@@ -0,0 +1,10 @@
+<html>
+
+<body>
+
+customer form...
+
+</body>
+
+
+</html>
--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -34,4 +34,11 @@ public class CustomerController {
 		
 		return "list-customers";
 	}
+	
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		return "customer-form";
+	}
 }
